9039bd16d5c91ed59662fddc2061dadad8779ca8
Vicky <vicky.bilbily@gmail.com>
2026-02-06 12:06:15 -0500

    attempted about page improvement + other responsive css improvements

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,16 +3,18 @@ import styles from './about.module.css'
 export function AboutPage() {
     return (
     <div className={styles.contentWrapper}>
-        <div className={styles.gridItem1}>
-            <img src="/media/vicky-portrait-square-overflow.png" alt="Vicky Bilbily self-portrait" title="Self-portrait by me!"/>
-        </div>
-        <div className={styles.gridItem2 + " fade-in"}>
-            <div className={styles.aboutGrid}>
-                <div className={styles.aboutContent}>
-                    <h1>Hi! I'm Vicky.</h1>
+        <div className={styles.grid}>
+            <div className={styles.gridItem1}>
+                <img src="/media/vicky-portrait-square-overflow.png" alt="Vicky Bilbily self-portrait" title="Self-portrait by me!"/>
+            </div>
+            <div className={styles.gridItem2 + " fade-in"}>
+                <div className={styles.aboutGrid}>
+                    <div className={styles.aboutContent}>
+                        <h1>Hi! I'm Vicky.</h1>
 
-                    I love to make things. I draw from expertise in software engineering, human-computer interaction,
-                    and visual art to create experiences that are reliable, intentional, and delightful.  
+                        I love to make things. I draw from expertise in software engineering, human-computer interaction,
+                        and visual art to create experiences that are reliable, intentional, and delightful.  
+                    </div>
                 </div>
             </div>
         </div>
